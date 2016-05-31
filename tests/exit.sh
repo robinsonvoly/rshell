c8518a0e8dcdd1e421b998b6516190380f03e332
rvoly001 <rvoly001@ucr.edu>
2016-05-30 19:35:57 -0700

    update exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-#Commands with preceding exit
-echo "Testing exit"
-exit
-
-echo "Testing logout"
-logout
-
-echo "Testing command followed by exit"
-ls -a
-exit
-
-echo hello
-exit
-
-echo "Testing connectors followed by exit"
-echo no || echo yea && echo this
-exit
-
-echo no; echo yea; echo this;
-exit
-
-echo "Testing exit with parentheses"
-(echo a || echo b)
-exit
+../bin/rshell < exit.txt
